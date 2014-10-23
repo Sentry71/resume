167eecd0268ad339547a8a71472751a00a7d47d5
Gary Hill <ghill315@att.net>
2014-10-22 22:11:12 -0500

    End of Lesson 1
    All the resume sections filled out

---
+++ js/resumeBuilder.js
@@ -0,0 +1,58 @@
+var work = {
+  "jobs" : [{
+    "employer": "AT&T",
+    "title" : "Senior IT Analyst",
+    "location" : "Earth City, MO, USA",
+    "dates" : "2000 - present",
+    "description" : "Programmer and web developer, specializing in full stack web development of tools for use by internal AT&T customer support teams."
+  }]
+};
+
+var projects = {
+  "projects" : [{
+    "title" : "Interactive Resume",
+    "dates" : 2014,
+    "description" : "Resume with data dynamically added to the web page.",
+    "images": ["images/197x148.gif","images/197x148.gif"]
+  },{
+    "title" : "Web Portfolio",
+    "dates" : 2014,
+    "description" : "Web page detailing projects finished with the Nanodegree courses.",
+    "images": ["images/197x148.gif","images/197x148.gif"]
+  }]
+};
+
+var bio = {
+  "name" : "Gary Hill",
+  "role" : "Web Developer",
+  "welcomeMessage" : "This is a welcome message - at least it's better than Hello World!",
+  "contacts" : {
+    "mobile" : "618-225-0351",
+    "email" : "ghill315@att.net",
+    "github" : "sentry71",
+    "location" : "Shiloh, IL, USA"
+  },
+  "skills" : ["HTML","Perl","Oracle"]
+};
+
+var education = {
+  "schools" : [{
+    "name" : "McKendree University",
+    "location" : "Lebanon, IL, USA",
+    "degree" : "BS",
+    "majors" : ["biology"],
+    "dates" : "1989 - 1993",
+    "url" : "http://www.mckendree.edu"
+  }],
+  "onlineCourses" : [{
+    "title" : "Intro to Java Programming",
+    "school" : "Udacity",
+    "dates" : 2014,
+    "url" : "https://www.udacity.com/course/cs046"
+  },{
+    "title" : "Try Git",
+    "school" : "Code School",
+    "dates" : 2014,
+    "url" : "https://try.github.io"
+  }]
+};
